05ad8c6d9bba4160530bef42a53b4dce227383de
KpdsK <KpdsK@users.noreply.github.com>
2021-12-05 18:02:42 -0300

    Pablo (#11)
    * 1er commit
    
    * Limpieza de repo
    
    * limpieza repo
    
    * Se modifica estado de modulo Cola, se agrega estructura Consumidor y se suscriben y desuscriben en la cola
    
    * Avance con cola RoundRobin (cola de trabajo)
    
    * se modifica el indice cuando un consumidor se desuscribe
    
    * Se modifican los timestamps de los mensajes y de los consumidores, para que refleje el momento donde ingresan a la cola, ademas se agrega la cola pu-sub
    
    * Toma el timestamp en el momento de la suscripcion
    
    * Merge MessageQueue
    
    * MessageQueue firms con map, version funcionando
    
    Co-authored-by: Iñaki Yabar <inayabarb@gmail.com>

--- lib/QueueManager.ex
+++ lib/QueueManager.ex
@@ -1,79 +1,88 @@
 defmodule QueueManager do
-    use GenServer
-    require Logger
+  use GenServer
+  require Logger
 
-    #---------------- Servidor ------------------#
+  # ---------------- Servidor ------------------#
 
-    def start_link(state)do
-      GenServer.start_link(__MODULE__, state, name: __MODULE__)
-    end
+  def start_link(state) do
+    GenServer.start_link(__MODULE__, state, name: __MODULE__)
+  end
 
-    def init(state) do
-        {:ok, state}
-    end
+  def init(state) do
+    {:ok, state}
+  end
 
-    # Queues
-    def handle_call(:get_queues, _from, state) do
-      #todo: how to get all distinct keys of MessageQueueRegistry?
-      #keys = Registry.keys(MessageQueueRegistry, self())
-      {:reply, :ok, state}
-    end
+  # Queues
+  def handle_call(:get_queues, _from, state) do
+    # todo: how to get all distinct keys of MessageQueueRegistry?
+    # keys = Registry.keys(MessageQueueRegistry, self())
+    {:reply, :ok, state}
+  end
 
-    #Producer
-    def handle_call({:subs_prod,}, from, state) do
-      IO.puts "Subscribing new producer with pid #{from}"
-      # Save producer pid for unsubscribe if need
-      queue_pids = [] # <- Queue pids who will receive messages from producer
-      {:replay, queue_pids,state}
-    end
+  # Producer
+  def handle_call({:subs_prod}, from, state) do
+    IO.puts("Subscribing new producer with pid #{from}")
+    # Save producer pid for unsubscribe if need
+    # <- Queue pids who will receive messages from producer
+    queue_pids = []
+    {:replay, queue_pids, state}
+  end
 
-    def handle_call({:create, queue_id, type}, _from, state) do
-      # TODO: Vincular con Colas
-      {:ok, pid} = MessageQueueDynamicSupervisor.start_child(queue_id, type, [])
-      Enum.each(Node.list(), fn node -> :rpc.call(node, QueueManager, :create_queue, [queue_id, type, :replicated]) end)
-      {:reply, pid, state}
-    end
+  def handle_call({:create, queue_id, type}, _from, state) do
+    # TODO: Vincular con Colas
+    {:ok, pid} = MessageQueueDynamicSupervisor.start_child(queue_id, type, [])
 
-    def handle_call({:create, queue_id, type, :replicated}, _from, state) do
-      # TODO: Vincular con Colas
-      {:ok, pid} = MessageQueueDynamicSupervisor.start_child(queue_id, type, [])
-      {:reply, pid, state}
-    end
+    Enum.each(Node.list(), fn node ->
+      :rpc.call(node, QueueManager, :create_queue, [queue_id, type, :replicated])
+    end)
 
-    def handle_cast({:delete, _queue_id}, state) do
-    #Todo: terminate procees, how to avoid supervisor to init again the queue, without changing strategy
-    #send message to terminate normally to the queue
-      {:noreply, state}
-    end
+    {:reply, pid, state}
+  end
 
-    # Consumers
-    def handle_cast({:subscribe, consumer_pid, queue_id, mode}, state) do
-      Logger.info("QM: Register #{inspect consumer_pid} to #{queue_id}")
+  def handle_call({:create, queue_id, type, :replicated}, _from, state) do
+    # TODO: Vincular con Colas
+    {:ok, pid} = MessageQueueDynamicSupervisor.start_child(queue_id, type, [])
+    {:reply, pid, state}
+  end
 
-      # Propagate subscription to all connected nodes
-      Enum.each(Node.list(), fn node -> GenServer.cast({QueueManager, node}, {:subscribe_replicate, consumer_pid, queue_id, mode}) end)
+  def handle_cast({:delete, _queue_id}, state) do
+    # Todo: terminate procees, how to avoid supervisor to init again the queue, without changing strategy
+    # send message to terminate normally to the queue
+    {:noreply, state}
+  end
 
-      # Subscribe consumer
-      do_subscribe(queue_id, consumer_pid, mode)
-      {:noreply, state}
-    end
+  # Consumers
+  def handle_cast({:subscribe, consumer_pid, queue_id, mode}, state) do
+    #Logger.info("QM: Register #{inspect(consumer_pid)} to #{queue_id}"
 
-    def handle_cast({:subscribe_replicate, consumer_pid, queue_id, mode}, state) do
-      do_subscribe(queue_id, consumer_pid, mode)
-      {:noreply, state}
-    end
+    # Propagate subscription to all connected nodes
+    Enum.each(Node.list(), fn node ->
+      GenServer.cast({QueueManager, node}, {:subscribe_replicate, consumer_pid, queue_id, mode})
+    end)
 
-    def handle_cast({:unsubscribe, consumer_pid, queue_id, mode}, state) do
-      Logger.info("QM: Unsubscribing #{inspect consumer_pid} from #{queue_id}")
+    # Subscribe consumer
+    do_subscribe(queue_id, consumer_pid, mode)
+    {:noreply, state}
+  end
 
-      # Propagate subscription to all connected nodes
-      Enum.each(Node.list(), fn node -> GenServer.cast({QueueManager, node}, {:unsubscribe, consumer_pid, queue_id, mode}) end)
+  def handle_cast({:subscribe_replicate, consumer_pid, queue_id, mode}, state) do
+    do_subscribe(queue_id, consumer_pid, mode)
+    {:noreply, state}
+  end
 
-      # Unsubscribe consumer from Registry
-      ConsumersRegistry.unsubscribe_consumer(queue_id, consumer_pid)
+  def handle_cast({:unsubscribe, consumer_pid, queue_id, mode}, state) do
+    #Logger.info("QM: Unsubscribing #{inspect(consumer_pid)} from #{queue_id}")
+    # Unsubscribe consumer from Registry
+    ConsumersRegistry.unsubscribe_consumer(queue_id, consumer_pid)
 
-      {:noreply, state}
-    end
+    # Propagate subscription to all connected nodes
+    Enum.each(Node.list(), fn node ->
+      GenServer.cast({QueueManager, node}, {:unsubscribe, consumer_pid, queue_id, mode})
+    end)
+
+    {:noreply, state}
+  end
+
 
     # TODO: Si soy el nodo activo, tengo que mandarselo a la cola y guardar en registry. Si no, solo lo guardo en el registry
     defp do_subscribe(queue_id, consumer_pid, mode) do
@@ -84,30 +93,34 @@ defmodule QueueManager do
       GenServer.cast(via_tuple, {:add_subscriber, %ConsumerStruct{id: consumer_pid}})
     end
 
-    #---------------- Cliente ------------------#
-
-    def get_queues() do
-      GenServer.call(QueueManager, :get_queues)
-    end
-
-
-    def create_queue(queue_id, type) do
-      GenServer.call(QueueManager, {:create, queue_id, type})
-    end
-
-    def create_queue(queue_id, type, :replicated) do
-      GenServer.call(QueueManager, {:create, queue_id, type, :replicated})
-    end
-
-    def delete_queue(queue_id) do
-      GenServer.cast(QueueManager, {:delete, queue_id})
-    end
-
-    def subscribe(consumer_pid, queue_id, mode) do
-      GenServer.cast(QueueManager, {:subscribe, consumer_pid, queue_id, mode})
-    end
-
-    def unsubscribe(consumer_pid, queue_id) do
-      GenServer.cast(QueueManager, {:unsubscribe, consumer_pid, queue_id})
-    end
+  # TODO: Si soy el nodo activo, tengo que mandarselo a la cola y guardar en registry. Si no, solo lo guardo en el registry
+  defp do_subscribe(queue_id, consumer_pid, mode) do
+    MessageQueueRegistry.subscribe_consumer(queue_id, consumer_pid, mode)
   end
+
+  # ---------------- Cliente ------------------#
+
+  def get_queues() do
+    GenServer.call(QueueManager, :get_queues)
+  end
+
+  def create_queue(queue_id, type) do
+    GenServer.call(QueueManager, {:create, queue_id, type})
+  end
+
+  def create_queue(queue_id, type, :replicated) do
+    GenServer.call(QueueManager, {:create, queue_id, type, :replicated})
+  end
+
+  def delete_queue(queue_id) do
+    GenServer.cast(QueueManager, {:delete, queue_id})
+  end
+
+  def subscribe(consumer_pid, queue_id, mode) do
+    GenServer.cast(QueueManager, {:subscribe, consumer_pid, queue_id, mode})
+  end
+
+  def unsubscribe(consumer_pid, queue_id) do
+    GenServer.cast(QueueManager, {:unsubscribe, consumer_pid, queue_id})
+  end
+end
